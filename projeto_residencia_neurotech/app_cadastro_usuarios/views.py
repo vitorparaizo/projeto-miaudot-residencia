0fd38fd1910e65f4dcc2d3e1b9865cc5a20bcb91
Vitor Paraizo <103430661+vitorparaizo@users.noreply.github.com>
2025-05-08 21:39:45 -0300

    criando views e alteração de urls

--- projeto_residencia_neurotech/app_cadastro_usuarios/views.py
+++ projeto_residencia_neurotech/app_cadastro_usuarios/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 from .models import Usuario
 
@@ -17,4 +19,20 @@ def usuarios(request):
     }
 
     return render(request, 'usuarios.html', usuarios)
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('login')
+        else:
+            login_form = AuthenticationForm
+    else:  
+        login_form = AuthenticationForm
+        return render(request, 'login/login.html', {'login_form':login_form})
+
+
 
--- projeto_residencia_neurotech/projeto_residencia_neurotech/urls.py
+++ projeto_residencia_neurotech/projeto_residencia_neurotech/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path 
 from app_cadastro_usuarios import views
+from app_cadastro_usuarios.views import login_view
+from django.contrib import admin
 
 urlpatterns = [
     # rota , view , responsavel, nome 
 
     path('',views.home, name="home"),
-    path('usuarios/', views.usuarios, name='listagem_usuarios')
+    path('admin/', admin.site.urls),
+    path('usuarios/', views.usuarios, name='listagem_usuarios'),
+    path('login/', login_view, name='login')
     
 ]
