e09a221b0fcbe57cc04869788b964f986d1a6012
Vitor Paraizo <103430661+vitorparaizo@users.noreply.github.com>
2025-05-10 13:46:47 -0300

    fazendo rotas de autenticação de usuarios

--- projeto_residencia_neurotech/app_cadastro_usuarios/views.py
+++ projeto_residencia_neurotech/app_cadastro_usuarios/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Usuario
 
 
@@ -20,6 +20,19 @@ def usuarios(request):
 
     return render(request, 'usuarios.html', usuarios)
 
+def register_view(request):
+    if request.method == "POST":
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('usuarios')
+    else:
+        user_form = UserCreationForm()
+    return render(request, 'register.html', {'user_form':user_form})
+
+    user_form = UserCreationForm()
+    return render(request, 'login/register.html', {'user_form':user_form})
+
 def login_view(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -27,7 +40,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('login')
+            return redirect('login/register.html')
         else:
             login_form = AuthenticationForm
     else:  
--- projeto_residencia_neurotech/projeto_residencia_neurotech/urls.py
+++ projeto_residencia_neurotech/projeto_residencia_neurotech/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path 
 from app_cadastro_usuarios import views
-from app_cadastro_usuarios.views import login_view
+from app_cadastro_usuarios.views import login_view, register_view
 from django.contrib import admin
 
 urlpatterns = [
@@ -9,6 +9,8 @@ urlpatterns = [
     path('',views.home, name="home"),
     path('admin/', admin.site.urls),
     path('usuarios/', views.usuarios, name='listagem_usuarios'),
-    path('login/', login_view, name='login')
+    path('login/', login_view, name='login'),
+    path('register/', register_view, name='register')
     
 ]
+
