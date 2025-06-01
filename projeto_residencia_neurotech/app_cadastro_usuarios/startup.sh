a2c2f49979d0667b20e6a54b1e5f0146d468d978
Vitor Paraizo <103430661+vitorparaizo@users.noreply.github.com>
2025-06-01 19:13:19 -0300

    ajustes para deploy Azure

---
+++ projeto_residencia_neurotech/app_cadastro_usuarios/startup.sh
@@ -0,0 +1,1 @@
+gunicorn projeto_residencia_neurotech.wsgi:application --bind 0.0.0.0 --timeout 600
--- projeto_residencia_neurotech/projeto_residencia_neurotech/settings.py
+++ projeto_residencia_neurotech/projeto_residencia_neurotech/settings.py
@@ -134,6 +134,6 @@ STATICFILES_DIRS = [
 
 
 #conf para azure 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['miaudote-e9e5eucdehhfcuaz.brazilsouth-01.azurewebsites.net']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
